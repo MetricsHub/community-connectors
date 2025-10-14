54f43eb1eb9ad24ae32ce09350536e742fb951c8
maalej <maalej@sentrysoftware.com>
2025-10-14 16:21:20 +0200

    Issue #275 Create emulation IT * Remove public keyword

--- src/it/java/ConnectorReplayIT.java
+++ src/it/java/ConnectorReplayIT.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class ConnectorReplayIT {
+class ConnectorReplayIT {
 
 	@ParameterizedTest
 	@ValueSource(strings = {
