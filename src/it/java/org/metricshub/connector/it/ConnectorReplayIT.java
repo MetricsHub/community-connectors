61ac738e4b543b034ee3e0933ba62d97ef983a76
Nassim Boutekedjiret <nassim@sentrysoftware.com>
2026-04-10 21:14:17 +0200

    Issue #368: New Emulation Framework - Update Integration Tests
    Updated SNMP ITs

--- src/it/java/org/metricshub/connector/it/ConnectorReplayIT.java
+++ src/it/java/org/metricshub/connector/it/ConnectorReplayIT.java
@@ -1,12 +1,15 @@
 package org.metricshub.connector.it;
 
-import java.nio.file.Paths;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Integration tests for various connectors using recorded data replay.
+ * Each test method corresponds to a single connector to minimize merge conflicts
+ * and provide clear JUnit output per connector.
  */
 class ConnectorReplayIT {
 
@@ -16,33 +19,142 @@ class ConnectorReplayIT {
 	 * @param connectorId The identifier of the connector to test
 	 * @throws Exception In case of any errors during the test execution
 	 */
-	@ParameterizedTest
-	@ValueSource(strings = {
-		//"AMDRadeon",
-		//"Cassandra",
-		//"IpmiTool",
-		//"Linux",
-		//"LinuxFile",
-		//"LinuxIfConfigNetwork",
-		//"LinuxIPNetwork",
-		//"LinuxIpmiTool",
-		//"Lmsensors",
-		//"MariaDB",
-		//"MIB2",
-		//"MIB2Switch",
-		//"MySQL",
-		//"PostgreSQL",
-		"Redfish",
-		//"SmartMonLinux",
-		//"WbemGenDiskNT",
-		//"WindowsFile",
-		//"WindowsIpmiTool",
-		//"WinStorageSpaces"
-	})
-	void testConnectorReplay(String connectorId) throws Exception {
+	private void testConnectorReplay(String connectorId) throws Exception {
 		new EmulationITBase(connectorId)
 			.withServerRecordData()
 			.executeStrategies()
 			.verifyExpected(connectorId + "/expected/expected.json");
+	}
+
+	/**
+	 * Writes the generated expected JSON output to a file for the specified connector.
+	 *
+	 * @param connectorId The identifier of the connector to generate expected JSON for
+	 * @throws Exception In case of any errors during JSON generation or file writing
+	 */
+	void writeExpectedJson(String connectorId) throws Exception {
+		final Path outputPath = Paths.get("src/it/resources/" + connectorId + "/expected/expected-gen.json");
+		outputPath.getParent().toFile().mkdirs();
+		new EmulationITBase(connectorId)
+			.withServerRecordData()
+			.executeStrategies()
+			.saveTelemetryManagerJson(outputPath);
+	}
+
+	@Test
+	@Disabled
+	void testAMDRadeon() throws Exception {
+		testConnectorReplay("AMDRadeon");
+	}
+
+	@Test
+	@Disabled
+	void testCassandra() throws Exception {
+		testConnectorReplay("Cassandra");
+	}
+
+	@Test
+	@Disabled
+	void testIpmiTool() throws Exception {
+		testConnectorReplay("IpmiTool");
+	}
+
+	@Test
+	@Disabled
+	void testLinux() throws Exception {
+		testConnectorReplay("Linux");
+	}
+
+	@Test
+	@Disabled
+	void testLinuxFile() throws Exception {
+		testConnectorReplay("LinuxFile");
+	}
+
+	@Test
+	@Disabled
+	void testLinuxIfConfigNetwork() throws Exception {
+		testConnectorReplay("LinuxIfConfigNetwork");
+	}
+
+	@Test
+	@Disabled
+	void testLinuxIPNetwork() throws Exception {
+		testConnectorReplay("LinuxIPNetwork");
+	}
+
+	@Test
+	@Disabled
+	void testLinuxIpmiTool() throws Exception {
+		testConnectorReplay("LinuxIpmiTool");
+	}
+
+	@Test
+	@Disabled
+	void testLmsensors() throws Exception {
+		testConnectorReplay("Lmsensors");
+	}
+
+	@Test
+	@Disabled
+	void testMariaDB() throws Exception {
+		testConnectorReplay("MariaDB");
+	}
+
+	@Test
+	void testMIB2() throws Exception {
+		testConnectorReplay("MIB2");
+	}
+
+	@Test
+	void testMIB2Switch() throws Exception {
+		testConnectorReplay("MIB2Switch");
+	}
+
+	@Test
+	@Disabled
+	void testMySQL() throws Exception {
+		testConnectorReplay("MySQL");
+	}
+
+	@Test
+	@Disabled
+	void testPostgreSQL() throws Exception {
+		testConnectorReplay("PostgreSQL");
+	}
+
+	@Test
+	void testRedfish() throws Exception {
+		testConnectorReplay("Redfish");
+	}
+
+	@Test
+	@Disabled
+	void testSmartMonLinux() throws Exception {
+		testConnectorReplay("SmartMonLinux");
+	}
+
+	@Test
+	@Disabled
+	void testWbemGenDiskNT() throws Exception {
+		testConnectorReplay("WbemGenDiskNT");
+	}
+
+	@Test
+	@Disabled
+	void testWindowsFile() throws Exception {
+		testConnectorReplay("WindowsFile");
+	}
+
+	@Test
+	@Disabled
+	void testWindowsIpmiTool() throws Exception {
+		testConnectorReplay("WindowsIpmiTool");
+	}
+
+	@Test
+	@Disabled
+	void testWinStorageSpaces() throws Exception {
+		testConnectorReplay("WinStorageSpaces");
 	}
 }
